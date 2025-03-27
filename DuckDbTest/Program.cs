9df8721cfd83a2023d7492e372c8bb7ef6647edb
Gwymlas <smrszr63@gmail.com>
2025-03-27 23:51:57 +0400

    Initial commit

---
+++ DuckDbTest/Program.cs
@@ -0,0 +1,137 @@
+﻿using DuckDB.NET.Data;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+using Npgsql;
+
+namespace DuckDbTest;
+
+internal class Program
+{
+    private static void Main()
+    {
+        const string host = "localhost";
+        const int port = 5444;
+        const string dbname = "test";
+        const string user = "postgres";
+        const string password = "123";
+
+        var postgresConnectionStringForDuckDb =
+            $"host={host} port={port} dbname={dbname} user={user} password={password}";
+        var postgresConnectionString = $"Host={host};Port={port};Database={dbname};Username={user};Password={password}";
+
+        // Создание тестовой таблицы в PostgreSQL
+        CreatePostgresTable(postgresConnectionString);
+
+        using var duckDbConnection = new DuckDBConnection("Data Source=duck.db");
+        duckDbConnection.Open();
+
+        using var command = duckDbConnection.CreateCommand();
+        
+        // Подключение PostgreSQL к DuckDb в режиме только для чтения
+        command.CommandText = $"INSTALL postgres; LOAD postgres; ATTACH '{postgresConnectionStringForDuckDb}' AS postgres_db (TYPE postgres, READ_ONLY)";
+        command.ExecuteNonQuery();
+        
+        //Включение пространственного расширения
+        command.CommandText = "INSTALL spatial; LOAD spatial;";
+        command.ExecuteNonQuery();
+        
+        TransferDataToDuckDb(duckDbConnection);
+        
+        PrintDataToConsole(duckDbConnection);
+        
+        TestNtsForDuckDbData(duckDbConnection);
+        
+        duckDbConnection.Close();
+    }
+
+    private static void CreatePostgresTable(string connectionString)
+    {
+        using var postgresConnection = new NpgsqlConnection(connectionString);
+        postgresConnection.Open();
+
+        using var command = new NpgsqlCommand("CREATE EXTENSION IF NOT EXISTS postgis", postgresConnection);
+        command.ExecuteNonQuery();
+        
+        // Создание тестовой таблицы
+        command.CommandText = @"
+            CREATE TABLE IF NOT EXISTS geom_test (
+                id SERIAL PRIMARY KEY,
+                geom GEOMETRY
+            )";
+        command.ExecuteNonQuery();
+
+        command.CommandText = @"
+            INSERT INTO geom_test (geom) VALUES 
+            (ST_GeomFromText('POLYGON((19.65622766921524 54.4686443760213, 
+                                  19.65622766921524 54.46830195180331, 
+                                  19.656796427736964 54.46830195180331, 
+                                  19.656796427736964 54.4686443760213, 
+                                  19.65622766921524 54.4686443760213))', 4326)),
+            (ST_GeomFromText('POLYGON((50.17541970055876 53.21416021499951, 
+                                  50.17533053703244 53.2141271619239, 
+                                  50.17522014409445 53.214205980754855, 
+                                  50.17493142410382 53.21407631098768, 
+                                  50.17529232409254 53.21377120410435, 
+                                  50.17558104408323 53.21389578967748, 
+                                  50.17550886408509 53.21396952382588, 
+                                  50.1756065193764 53.21402291744005, 
+                                  50.17541970055876 53.21416021499951))', 4326))";
+        command.ExecuteNonQuery();
+    
+        // (ST_SetSRID(ST_MakePoint(20.51206169768136, 54.70643639899387), 4326))
+        Console.WriteLine("Тестовая таблица создана в PostgreSQL");
+        postgresConnection.Close();
+    }
+
+    private static void TransferDataToDuckDb(DuckDBConnection duckDbConnection)
+    {
+        using var command = duckDbConnection.CreateCommand();
+        
+        command.CommandText = "DROP TABLE IF EXISTS duckdb_geom";
+        command.ExecuteNonQuery();
+        
+        // Создание таблицы в DuckDB и вставка данных
+        command.CommandText = @"
+            CREATE TABLE duckdb_geom AS
+            SELECT id, ST_GeomFromWKB(geom) as geom FROM postgres_db.geom_test";
+        command.ExecuteNonQuery();
+    }
+
+    private static void PrintDataToConsole(DuckDBConnection duckDbConnection)
+    {
+        using var command = duckDbConnection.CreateCommand();
+        
+        command.CommandText = "SELECT id, ST_AsText(geom) as geom FROM duckdb_geom";
+        using var duckDbReader = command.ExecuteReader();
+        Console.WriteLine("\nДанные в DuckDB:");
+        while (duckDbReader.Read())
+            Console.WriteLine($"ID: {duckDbReader.GetInt32(0)}, Geometry: {duckDbReader.GetString(1)}");
+    }
+
+    private static void TestNtsForDuckDbData(DuckDBConnection duckDbConnection)
+    {
+        using var command = duckDbConnection.CreateCommand();
+
+        command.CommandText = "SELECT id, ST_AsWKB(geom) as geom_wkb FROM duckdb_geom";
+
+        var wkbReader = new WKBReader();
+        var geometries = new List<Geometry>();
+
+        Console.WriteLine("\nРабота с геометрией через NTS:");
+        using var duckDbReader = command.ExecuteReader();
+        while (duckDbReader.Read())
+        {
+            var wkbBytes = duckDbReader.GetStream(1);
+            var geometry = wkbReader.Read(wkbBytes);
+            geometries.Add(geometry);
+            
+            Console.WriteLine($"Тип геометрии: {geometry.GeometryType}, Площадь: {geometry.Area}");
+        }
+        
+        var distance = geometries[0].Distance(geometries[1]);
+        Console.WriteLine($"Расстояние между первым и вторым элементами: {distance}");
+        
+        var crosses = geometries[0].Crosses(geometries[1]);
+        Console.WriteLine($"Пересекает ли: {(crosses? "Да" : "Нет")}");
+    }
+}
